2cde43190e98a597dc882a3c9ae9d3a81ca88355
Fernando Huelin Parras <fernanh98@gmail.com>
2025-11-25 17:45:09 +0100

    add bag of words

---
+++ src/words.py
@@ -0,0 +1,7 @@
+WORDS = [
+    "banana", "car", "game", "card", "technology", "galaxy", "computer", 
+    "artificial", "science", "letter", "ship", "rider", "fire", "screen", 
+    "desktop", "darkness", "light", "sun", "moon", "number", "money",
+    "language", "english", "spanish", "poison", "german", "chinese", 
+    "towel", "water", "waterfall", "trash"
+]
