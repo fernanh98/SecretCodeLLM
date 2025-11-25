e590c00eb924c2cf8473f08fe1c5ec8c0632b1d5
Fernando Huelin Parras <fernanh98@gmail.com>
2025-11-25 17:45:56 +0100

    data model schemas

---
+++ src/schemas.py
@@ -0,0 +1,86 @@
+from typing import List, TypedDict, Literal, Dict, Any, Optional
+from pydantic import BaseModel
+
+
+class TeamWords(TypedDict):
+    red: List[str]
+    blue: List[str]
+    neutral: List[str]
+    black: str
+
+
+class LeftTeamWords(TypedDict):
+    red: List[str]
+    blue: List[str]
+    neutral: List[str]
+    black: str
+    left_words: List[str]
+    known: List[str]
+
+
+class Code(BaseModel):
+    word: str
+    number: int
+    justification: str
+    words_related: List[str]
+
+
+class Choice(BaseModel):
+    words: List[str]
+    justification: str
+
+
+# class BoardWords(TypedDict):
+#     red: List[str]
+#     blue: List[str]
+#     neutral: List[str]
+#     black: List[str]
+
+
+class PlayerData(BaseModel):
+    model_name: str
+    temperature: float
+    seed: int | None
+
+
+class TeamData(BaseModel):
+    captain: PlayerData
+    guesser: PlayerData
+
+
+class Teams(BaseModel):
+    blue: TeamData
+    red: TeamData
+
+
+class RoundTeamData(BaseModel):
+    secret_code: Code
+    guesser_choice: Choice
+    captain_prompt: Optional[str]
+    guesser_prompt: Optional[str]
+
+
+class Round(BaseModel):
+    round: int
+    blue_team: RoundTeamData
+    red_team: RoundTeamData
+
+
+class Results(BaseModel):
+    words: TeamWords
+    team_order: List[Literal["red", "blue"]]
+    teams: Teams
+    num_rounds: int
+    rounds_info: List[Round]
+    winner_team: Literal["red", "blue"]
+
+
+class ToolCall(BaseModel):
+    tool_name: str
+    args: Dict[str, Any]
+
+
+class LLMMessage(BaseModel):
+    model_name: str
+    content: Optional[str]
+    tool_call: Optional[ToolCall]
